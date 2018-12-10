f74589a2f4fc94c59168878041a0362810eb3636
hamidshj <hamid.sh.j@gmail.com>
2018-12-10 18:03:30 +0330

    hello

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,5 +1,32 @@
-public class Start {
-    public static void main(String[] args) {
+import com.github.davidmoten.rtree.RTree;
+import com.github.davidmoten.rtree.geometry.Geometries;
+import com.github.davidmoten.rtree.geometry.Point;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+import static com.github.davidmoten.rtree.geometry.Geometries.point;
+
+public class Start {
+    private static final String PRE_PATH = "/home/hamid/IdeaProjects/Clustering/src/main/resources/";
+
+    public static void main(String[] args) throws Exception {
+        readPoint(PRE_PATH + "input1");
+    }
+
+    public static RTree<Integer, Point> readPoint(String path) throws Exception {
+        RTree<Integer, Point> tree = RTree.create();
+        File file = new File(path);
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        int n = 0;
+        String st;
+        while ((st = br.readLine()) != null) {
+            String[] dim = st.split(" ");
+            tree=tree.add(++n, point(Integer.parseInt(dim[0]), Integer.parseInt(dim[0])));
+        }
+        tree.visualize(600,600)
+                .save(PRE_PATH+"/mytree.png");
+        return tree;
     }
 }
